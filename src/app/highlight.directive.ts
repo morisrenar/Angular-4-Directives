04b2c2e1960aec0f263f44390d33cc816ed2b019
bmshamsnahid <morisrenar@gmail.com>
2017-06-19 01:53:45 +0600

    42: error

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,21 +1,26 @@
-import {Directive, ElementRef, HostBinding, HostListener, Renderer} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[dirHighlight]'
 })
-export class HighlightDirective {
+export class HighlightDirective implements OnInit {
+  private backgroundColor: String;
+  @Input() defaultColor = 'white';
+  @Input('dirHighlight') highlightColor = 'green';
   @HostListener('mouseenter') mouseover() {
-    this.backgroundColor = 'green';
+    this.backgroundColor = this.highlightColor;
   };
   @HostListener('mouseleave') mouseleave() {
-    this.backgroundColor = 'white';
+    this.backgroundColor = this.defaultColor;
   };
   @HostBinding('style.backgroundColor') get setColor() {
     return this.backgroundColor;
   }
-  private backgroundColor = 'white';
   constructor(private elementRef: ElementRef, private renderer: Renderer) {
     /*this.elementRef.nativeElement.style.backgroundColor = 'green';*/
     /*this.renderer.setElementStyle(this.elementRef.nativeElement, 'background-color', 'green');*/
+  }
+  ngOnInit() {
+    this.backgroundColor = this.defaultColor;
   }
 }
