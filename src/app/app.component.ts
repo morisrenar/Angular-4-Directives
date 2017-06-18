77bdad69741c99e5176087f2940ce5a8dbd84d70
bmshamsnahid <morisrenar@gmail.com>
2017-06-19 02:20:29 +0600

    46: de-sugaring the ngIf and ngSwitch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {HighlightDirective} from './highlight.directive';
 export class AppComponent {
   private switch = true;
   private items = [1, 2, 3, 4, 5];
-  private value = 110;
+  private value = 10;
   onSwitch() {
     this.switch = !this.switch;
   }
